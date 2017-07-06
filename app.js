e11c5f8df419d286fc24afc525833a688cd9a2db
przemyslaw.lach <lprzemek@gmail.com>
2017-07-06 18:31:00 +0200

    added steps

--- app.js
+++ app.js
@@ -14,42 +14,46 @@ const createElement = (type, params = {}) => {
 
   return elem;
 };
-
+  // 11
 const clearNode = (node) => node.innerHTML = '';
 
 const people = (() => {
-
+  // 1
   const people = [];
-
+  // 2
   const el = document.querySelector('.peopleModule');
+  // 3
   const button = el.querySelector('button');
+  // 4
   const input = el.querySelector('input');
+  // 10
   const ul = el.querySelector('ul');
 
-
+  // 7
   const addPerson = (name) => {
     people.push(name);
     render();
     input.value = '';
   };
-
+  // 15
   const deletePerson = (index) => {
     people.splice(index, 1);
     render();
   };
-
+  // 6
   const addPersonHandler = () => {
     addPerson(input.value);
   };
-
+  // 14
   const deletePersonHandler = (event) => {
     const indexToRemove = people.indexOf(event.target.innerHTML);
     deletePerson(indexToRemove);
   };
-
+  // 5
   button.addEventListener('click', addPersonHandler);
+  // 13
   ul.addEventListener('click', deletePersonHandler);
-
+  // 9
   const getPersonHtmlItem = (name) => {
       const li = createElement('li');
       const span = createElement('span');
@@ -70,7 +74,7 @@ const people = (() => {
 
       return li;
   };
-
+  // 8
   const render = () => {
     clearNode(ul);
     people.forEach(name => {
@@ -78,9 +82,8 @@ const people = (() => {
       ul.appendChild(personHtmlItem);
     });
   };
-
-  render();
-
+  // 12
+  // 16
   return {
     addPerson,
     deletePerson,
